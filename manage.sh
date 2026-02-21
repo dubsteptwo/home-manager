180ca96ff38f4f3521f358925d919ffa566113b6
dubsteptwo <909froko@gmail.com>
2026-02-21 15:42:23 +0300

    improve script

--- manage.sh
+++ manage.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+curr_dir = "$(pwd)"
+cd /home/maple/home-manager
+
+
 if [[ $1 == "update" ]]; then
     home-manager switch --flake .#maple
 elif [[ $1 == "clean" ]]; then
     nix-collect-garbage -d
 fi
+
+cd "$curr_dir"
