ae6d80eea776f7cdcf370d70579434d3838e8704
dubsteptwo <909froko@gmail.com>
2026-02-23 16:43:40 +0300

    add rebuild command

--- manage.sh
+++ manage.sh
@@ -8,6 +8,8 @@ if [[ $1 == "update" ]]; then
     home-manager switch --flake .#maple
 elif [[ $1 == "clean" ]]; then
     nix-collect-garbage -d
+elif [[ $1 == "sysupdate" ]]; then
+    sudo nixos-rebuild switch
 fi
 
 cd "$curr_dir"
