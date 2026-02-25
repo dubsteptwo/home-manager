3e1be0574735353915e0ae04f4d1506cd907e365
dubsteptwo <909froko@gmail.com>
2026-02-25 22:17:47 +0300

    make it not insufferable

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -1,24 +1,25 @@
 { config, lib, ... }:
   {
     home.file = {
-    
+      # Helper script
+      "/home/maple/.local/bin/manage".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/scripts/manage.sh";
       # OpenTablet Artist pro 12 tweak
       "/home/maple/.local/share/OpenTabletDriver/Configurations/Artist.12.pro.json".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/OpenTabletDriver/Configurations/Artist.12.Pro.json";
     
       # Openbox config
-      "/home/maple/.config/openbox/autostart".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/openbox/autostart";
-      "/home/maple/.config/openbox/environment".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/openbox/environment";
-      "/home/maple/.config/openbox/menu.xml".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/openbox/menu.xml;
-      "/home/maple/.config/openbox/rc.xml".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/openbox/rc.xml;
-
+      #"/home/maple/.config/openbox/autostart".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/openbox/autostart";
+      #"/home/maple/.config/openbox/environment".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/openbox/environment";
+      #"/home/maple/.config/openbox/menu.xml".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/openbox/menu.xml;
+      #"/home/maple/.config/openbox/rc.xml".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/openbox/rc.xml;
+      "/home/maple/.config/openbox".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/openbox";
       # Picom config
-      "/home/maple/.config/picom/picom.conf".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/picom/picom.conf;
+      "/home/maple/.config/picom".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/picom";
 
       # rofi config
-      "/home/maple/.config/rofi/config.rasi".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/rofi/config.rasi;
+      "/home/maple/.config/rofi".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/rofi;
 
       # polybar config
-      "/home/maple/.config/polybar/config".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/polybar/config.ini;
+      "/home/maple/.config/polybar".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/polybar;
 
     };
   }
