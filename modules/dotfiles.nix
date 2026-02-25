8a1d5360469ef1106997f0af93b5118e598bfffb
dubsteptwo <909froko@gmail.com>
2026-02-25 23:14:36 +0300

    change openbox config

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -7,11 +7,11 @@
       "/home/maple/.local/share/OpenTabletDriver/Configurations/Artist.12.pro.json".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/OpenTabletDriver/Configurations/Artist.12.Pro.json";
     
       # Openbox config
-      #"/home/maple/.config/openbox/autostart".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/openbox/autostart";
-      #"/home/maple/.config/openbox/environment".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/openbox/environment";
-      #"/home/maple/.config/openbox/menu.xml".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/openbox/menu.xml;
-      #"/home/maple/.config/openbox/rc.xml".source = config.lib.file.mkOutOfStoreSymlink /home/maple/home-manager/dots/openbox/rc.xml;
       "/home/maple/.config/openbox".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/openbox";
+
+      # Openbox theme
+      "/home/maple/.themes/SBDots".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/themes/SBDots";
+
       # Picom config
       "/home/maple/.config/picom".source = config.lib.file.mkOutOfStoreSymlink "/home/maple/home-manager/dots/picom";
 
